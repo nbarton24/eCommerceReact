2796bfe7aa638aaf41cdef517a366ade0707ddfe
Nicholas Barton <n.barton@criteo.com>
2020-03-18 18:36:44 -0400

    Finished laying out the sign in form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage}/>
         <Route path="/shop" component={ShopPage}/>
+        <Route path="/signin" component={SignInAndSignUp}/>
       </Switch>
     </div>
   );
