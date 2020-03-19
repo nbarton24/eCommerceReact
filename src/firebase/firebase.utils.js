710c05173e6f5a7997fbd50626c6fcfb3a268a7b
Nicholas Barton <n.barton@criteo.com>
2020-03-19 11:53:26 -0400

    added firebase utils to the sign up form

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ const config = {
     appId: "1:624187451711:web:49eb1857d3635b55694b8a"
   };
 
+  export const createUserProfileDocument = async (userAuth, additonalData) => {
+    if(!userAuth) return;
+    
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+      const {displayName,email} = userAuth;
+      const createdAt = new Date();
+
+      try{
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additonalData
+        })
+      }catch (error){
+        console.log('error creating user',error.message);
+      }
+    }
+    return userRef;
+
+  }
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
