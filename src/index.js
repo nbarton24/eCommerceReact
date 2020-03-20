832c4d7472632d810da7a2bc9f590590512b6f9e
Nicholas Barton <n.barton@criteo.com>
2020-03-20 17:40:45 -0400

    separated out data into it's own file, initial state now built by reducers

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react'
 import {BrowserRouter} from 'react-router-dom';
-import store from './redux/store.js';
+import {store,persistor} from './redux/store.js';
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App />
+            <PersistGate persistor={persistor}>
+                <App />        
+            </PersistGate>
         </BrowserRouter>
     </Provider>, 
     document.getElementById('root')
