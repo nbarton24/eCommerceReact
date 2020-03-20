c6f422fab381f8ea97ea52a40ff4b9643598001a
Nicholas Barton <n.barton@criteo.com>
2020-03-20 18:45:12 -0400

    added in collection pages. Nothing is displayed yet

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import CollectionItem from '../../components/collection-item/collection-item.component';
+import {connect} from 'react-redux'
+
+import {selectCollection} from '../../redux/shop/shop.selectors';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ collection }) => {
+  console.log(collection.items)
+  return (
+    <div className='collection-page'>
+      <h2 className='title'>{collection.title}</h2>
+      
+    </div>
+  );
+};
+
+const mapStateToProps = (state,ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage);
